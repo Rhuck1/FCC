e2dad69fcf40685dc0ac90d8ab9998682c3e944d
Rhuck1 <bobbyhuck@gmail.com>
2022-09-18 16:38:28 -0700

    first practice commits

---
+++ exercise_02_02/exercise_02_02.py
@@ -0,0 +1,8 @@
+'''
+A program that uses input to prompt a user for their name and then welcomes them. 
+Note that input will pop up a dialog box. 
+Enter <name> in the pop-up box when you are prompted so your output will match the desired output.
+'''
+
+npt = input('Enter your name: ')
+print('Hello', npt)
