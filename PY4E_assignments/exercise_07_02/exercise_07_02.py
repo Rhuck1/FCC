49182366b3cebe46db702a75a32f445d9907a4e5
Rhuck1 <bobbyhuck@gmail.com>
2022-09-21 12:48:55 -0700

    opening files and returning results

--- PY4E_assignments/exercise_07_02/exercise_07_02.py
+++ PY4E_assignments/exercise_07_02/exercise_07_02.py
@@ -11,3 +11,34 @@ Do not use the sum() function or a variable named sum in your solution.
 You can download the sample data at http://www.py4e.com/code3/mbox-short.txt,
  when you are testing below enter mbox-short.txt as the file name.
 '''
+
+file_name = input("Enter file name: ")
+
+try:
+    file_handle = open(file_name)
+
+except:
+    print('File cannot be opened:', file_name)
+    quit()
+
+count = 0
+spam_count = 0
+
+for line in file_handle:
+
+    line = line.rstrip()
+
+    if not line.startswith("X-DSPAM-Confidence"):
+        continue
+
+    count += 1
+    col_pos = text.find(':')
+
+    iso_text = text[col_pos + 1:]
+
+    flt_val = float(iso_text)
+
+    spam_count += flt_val
+
+
+
