021776626e5cd5e799b38d5aa880aafc060b6a18
Rhuck1 <bobbyhuck@gmail.com>
2022-10-07 05:13:27 -0700

    working on exercise 11

--- PY4E_assignments/exercise_11/exercise_11.py
+++ PY4E_assignments/exercise_11/exercise_11.py
@@ -2,5 +2,40 @@
 A program to read through and parse a file with text and numbers. 
 To extract all the numbers in the file and compute the sum of the numbers. 
 '''
+import re
 
-print('test')
+file_name = input("Enter file name: ")
+
+if len(file_name) < 1:
+    file_name = 'test.txt'
+
+try:
+    file_handle = open(file_name)
+
+except:
+    print("File cannot be opened:", file_name)
+    quit()
+
+count = 0
+
+for line in file_handle:
+
+    line = line.rstrip()
+    
+    if re.search('[0-9]+', line):
+        
+        line_nums = re.findall('[0-9]+', line)
+
+        sum = 0
+
+        for str_num in line_nums:
+          
+            sum += int(str_num)
+
+        count += sum
+
+print(count)
+
+
+
+    
