65ef80ddcfd54e65d5d466e4ecaeba2ef07195e8
Rhuck1 <bobbyhuck@gmail.com>
2022-10-06 19:56:28 -0700

    working on exercise 10

---
+++ PY4E_assignments/exercise_10_02/exercise_10_02.py
@@ -0,0 +1,41 @@
+'''
+A program to read through the mbox-short.txt and figure out the
+ distribution by hour of the day for each of the messages. 
+You can pull the hour out from the 'From ' line by finding the time 
+and then splitting the string a second time using a colon.
+
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+
+Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
+'''
+
+file_name = input("Enter file name: ")
+
+try:
+    file_handle = open(file_name)
+
+except:
+    print("File cannot be opened:", file_name)
+    quit()
+
+d = {}
+
+for line in file_handle:
+
+    line = line.rstrip()
+
+    if not line.startswith("From "):
+        continue
+
+    split_line = line.split()
+
+    for word in split_line:
+
+        if ':' not in word:
+            continue
+
+        time = word.split(':')
+        hour = time[0]
+
+        print(type(time[0]))
+
