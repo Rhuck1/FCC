14e9def1b494ff5f985624bc12d7499dfa2223b1
Rhuck1 <bobbyhuck@gmail.com>
2022-10-13 14:15:33 -0700

    finished exercise 12_02

--- PY4E_assignments/exercise_12_02/exercise_12_02.py
+++ PY4E_assignments/exercise_12_02/exercise_12_02.py
@@ -17,15 +17,23 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = 'http://www.dr-chuck.com/'
 
-# url = 'http://py4e-data.dr-chuck.net/comments_42.html'
-# url = input('Enter URL: ')
+url = input('Enter URL: ')
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, 'html.parser')
 
-# Retrieve all of the anchor tags
-tags = soup('a')
-for tag in tags:
-    print(tag.get('href', None))
+count = 0
 
+# Retrieve all of the anchor tags
+tags = soup('span')
+for tag in tags:
+
+    # Loops through tag and prints desired tag aspect
+    # print('TAG:', tag)
+    # print('URL:', tag.get('href', None))
+    # print('Contents:', tag.contents[0])
+    # print('Attrs:', tag.attrs)
+
+    count += int(tag.contents[0])
+
+print(count)
