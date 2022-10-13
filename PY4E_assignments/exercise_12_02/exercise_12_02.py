7284a179a457278f30597c497caf33d354059abc
Rhuck1 <bobbyhuck@gmail.com>
2022-10-13 12:25:14 -0700

    working on exercise 12_02

--- PY4E_assignments/exercise_12_02/exercise_12_02.py
+++ PY4E_assignments/exercise_12_02/exercise_12_02.py
@@ -10,10 +10,22 @@ Files below:
 
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
+import ssl
 
-samp_url = 'http://py4e-data.dr-chuck.net/comments_42.html'
-url = 'http://py4e-data.dr-chuck.net/comments_1650653.html'
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
-file_handle = urllib.request.urlopen(samp_url)
+url = 'http://www.dr-chuck.com/'
 
-for line in file_handle: print(line.decode().strip())
+# url = 'http://py4e-data.dr-chuck.net/comments_42.html'
+# url = input('Enter URL: ')
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+# Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
