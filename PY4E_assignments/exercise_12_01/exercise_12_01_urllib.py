0ef7403fb6a6f79a4b9007a3b6ebc4eef00f57a3
Rhuck1 <bobbyhuck@gmail.com>
2022-10-12 20:41:45 -0700

    finished exercise 12 with 2 approaches

--- PY4E_assignments/exercise_12_01/exercise_12_01_urllib.py
+++ PY4E_assignments/exercise_12_01/exercise_12_01_urllib.py
@@ -12,6 +12,28 @@ url = 'http://data.pr4e.org/intro-short.txt'
 
 file_handle = urllib.request.urlopen(url)
 
-for line in file_handle:
+# This is the code to print the headers requested per the assignment
+headers = file_handle.getheaders()
 
-    print(line.decode().strip())
+for head in headers:
+
+    if head[0] == 'Last-Modified': 
+        print('Last-Modified:', head[1])
+
+    elif head[0] == 'ETag':
+        print('ETag:', head[1])
+
+    elif head[0] == 'Content-Length':
+        print('Content-Length:', head[1])
+
+    elif head[0] == 'Cache-Control':
+        print('Cache-Control:', head[1])
+
+    elif head[0] == 'Content-Type':
+        print('Content-Type:', head[1])
+
+
+# Below code will print out only txt from url    
+# for line in file_handle:
+
+#     print(line.decode().strip())
