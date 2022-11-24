2e37a0d603bc50d26ac79a09ad23161ec6f70320
Rhuck1 <bobbyhuck@gmail.com>
2022-11-24 14:12:48 -0700

    finished

--- PY4E_assignments/exercise_12_03.py
+++ PY4E_assignments/exercise_12_03.py
@@ -28,73 +28,32 @@ url = input('Enter URL: ')
 process = int(input('Enter process loops: '))
 position = int(input('Enter position: '))
 
-# print(f'Retrieving: {url}')
-
-# url_list = []
-# tag_list = []
-# content_list = []
-
-# for index in range(process):
-
-#     html = urllib.request.urlopen(url, context=ctx).read()
-#     soup = BeautifulSoup(html, 'html.parser')
-    
-#     tags = soup('a')
-
-#     process_counter = 0
-#     position_counter = 0
-
-#     for tag in tags:
-
-#         position_counter += 1
-#         if position_counter == position:
-#             url = str(tag.get('href', None))
-#             print(f'Retrieving: {url}')
-#             url_list.append(url)
-#             tag_list.append(tag)
-#             content_list.append(tag.contents[0])
-#             position_counter = 0
-#             break
-
-# print("-----------------------------")
-# print('URL List:', url_list)
-# print("-----------------------------")
-# print('Tag List:', tag_list)
-# print("-----------------------------")
-# print('Content List:', content_list)
-# print("-----------------------------")
-# print('Final Name:', content_list[-1])
-
-'''
-The program below uses a while loop to accomplish the task
-'''
+print(f'Retrieving: {url}')
 
 url_list = []
 tag_list = []
 content_list = []
 
-process_count = 0
-
-while process_count < process:
+for index in range(process):
 
     html = urllib.request.urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, 'html.parser')
+    
     tags = soup('a')
 
+    process_counter = 0
     position_counter = 0
 
     for tag in tags:
-        
+
         position_counter += 1
         if position_counter == position:
-
-            print('Retrieving: ', url)
             url = str(tag.get('href', None))
+            print(f'Retrieving: {url}')
             url_list.append(url)
             tag_list.append(tag)
             content_list.append(tag.contents[0])
             position_counter = 0
-            process_count += 1
             break
 
 print("-----------------------------")
@@ -105,3 +64,44 @@ print("-----------------------------")
 print('Content List:', content_list)
 print("-----------------------------")
 print('Final Name:', content_list[-1])
+
+'''
+The program below uses a while loop to accomplish the task
+'''
+
+# url_list = []
+# tag_list = []
+# content_list = []
+
+# process_count = 0
+
+# while process_count < process:
+
+#     html = urllib.request.urlopen(url, context=ctx).read()
+#     soup = BeautifulSoup(html, 'html.parser')
+#     tags = soup('a')
+
+#     position_counter = 0
+
+#     for tag in tags:
+        
+#         position_counter += 1
+#         if position_counter == position:
+
+#             print('Retrieving: ', url)
+#             url = str(tag.get('href', None))
+#             url_list.append(url)
+#             tag_list.append(tag)
+#             content_list.append(tag.contents[0])
+#             position_counter = 0
+#             process_count += 1
+#             break
+
+# print("-----------------------------")
+# print('URL List:', url_list)
+# print("-----------------------------")
+# print('Tag List:', tag_list)
+# print("-----------------------------")
+# print('Content List:', content_list)
+# print("-----------------------------")
+# print('Final Name:', content_list[-1])
