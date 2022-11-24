91a1cdcd187d62475a15f5f8e2f4e61b13093206
Rhuck1 <bobbyhuck@gmail.com>
2022-11-24 12:16:16 -0700

    final touches on 12.3

--- PY4E_assignments/exercise_12_03.py
+++ PY4E_assignments/exercise_12_03.py
@@ -8,8 +8,6 @@ Files below:
     Sample data: http://py4e-data.dr-chuck.net/known_by_Fikret.html (Last name in sequence: Anayah)
     Actual data: http://py4e-data.dr-chuck.net/known_by_Ashleigh.html (The first character of the name of the last page that you will load is: O)
 
-
-
 '''
 
 from socketserver import BaseRequestHandler
@@ -22,14 +20,13 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+'''
+The program below uses a range loop to accomplish the task
+'''
 
-#url = 'http://py4e-data.dr-chuck.net/known_by_Fikret.html'
-url = 'http://py4e-data.dr-chuck.net/known_by_Ashleigh.html'
-process = 7
-position = 18
-#url = input('Enter URL: ')
-#process = input('Enter process loops: ')
-#position = input('Enter position: ')
+url = input('Enter URL: ')
+process = input('Enter process loops: ')
+position = input('Enter position: ')
 
 # print(f'Retrieving: {url}')
 
@@ -68,11 +65,9 @@ position = 18
 # print("-----------------------------")
 # print('Final Name:', content_list[-1])
 
-    # Loops through tag and prints desired tag aspect
-    # print('TAG:', tag)
-    # print('URL:', tag.get('href', None))
-    # print('Contents:', tag.contents[0])
-    # print('Attrs:', tag.attrs)
+'''
+The program below uses a while loop to accomplish the task
+'''
 
 url_list = []
 tag_list = []
@@ -82,30 +77,31 @@ process_count = 0
 
 while process_count < process:
 
-    for index in range(process):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
 
-        html = urllib.request.urlopen(url, context=ctx).read()
-        soup = BeautifulSoup(html, 'html.parser')
-        tags = soup('a')
- 
-        for tag in tags:
-            
+    position_counter = 0
+
+    for tag in tags:
+        
+        position_counter += 1
+        if position_counter == position:
+
             print('Retrieving: ', url)
+            url = str(tag.get('href', None))
+            url_list.append(url)
             tag_list.append(tag)
-#             url = str(tag.get('href', None))
-#             print(f'Retrieving: {url}')
-#             url_list.append(url)
-#             tag_list.append(tag)
-#             content_list.append(tag.contents[0])
-#             position_counter = 0
-#             break
+            content_list.append(tag.contents[0])
+            position_counter = 0
+            process_count += 1
+            break
 
-    url = tag_list[position-1].get('href', None)
-    url_list.append(url)
-
-    process_count += 1
-    
-
-print("url list:", url_list)
-print("last url:", url)
-print("tag list:", tag_list)
+print("-----------------------------")
+print('URL List:', url_list)
+print("-----------------------------")
+print('Tag List:', tag_list)
+print("-----------------------------")
+print('Content List:', content_list)
+print("-----------------------------")
+print('Final Name:', content_list[-1])
