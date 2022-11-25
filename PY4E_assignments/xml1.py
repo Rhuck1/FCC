6c8c1d9e7e2846bd706390e6893bd995b3e1434b
Rhuck1 <bobbyhuck@gmail.com>
2022-11-25 10:54:45 -0700

    xml assignment

---
+++ PY4E_assignments/xml1.py
@@ -0,0 +1,19 @@
+'''
+A simple program that utilizes the XML parser ElementTree to 
+demonstrate the parsing of a simple XML string.
+'''
+import xml.etree.ElementTree as ET
+
+data = '''
+<person>
+    <name>Chuck</name>
+    <phone type="intl">
+        +1 734 303 4456
+    </phone>
+    <email hide="yes"/>
+</person>'''
+
+tree = ET.fromstring(data)
+print('Name:', tree.find('name').text)
+print('Attr:', tree.find('email').get('hide'))
+
---
+++ PY4E_assignments/xml2.py
@@ -0,0 +1,28 @@
+'''
+A simple program that utilizes the XML parser ElementTree to 
+demonstrate the parsing of a simple XML string.
+'''
+import xml.etree.ElementTree as ET
+
+input = '''
+<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+    </users>
+</stuff>'''
+
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+
+for item in lst:
+    print('Name:', item.find('name').text)
+    print('Id:', item.find('id').text)
+    print('Attribute:', item.get("x"))
