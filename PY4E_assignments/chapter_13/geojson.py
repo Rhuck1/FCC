bcc3108565f543b664a9afd05b8479e6f223427f
Rhuck1 <bobbyhuck@gmail.com>
2022-11-27 14:46:51 -0700

    meow

--- PY4E_assignments/chapter_13/geojson.py
+++ PY4E_assignments/chapter_13/geojson.py
@@ -3,39 +3,54 @@ A simple program that utilizes the json parser to
 demonstrate the parsing of simple a geojson string.
 '''
 
-from tempfile import TemporaryFile
 import urllib.request, urllib.parse, urllib.error
 import json
+import ssl
 
-# Google is increasingly requiring keys for this API
-serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+api_key = False
+# If you have a Google Places API key, enter it here
+# api_key = 'AIzaSy___IDByT70'
+# https://developers.google.com/maps/documentation/geocoding/intro
+
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else :
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 while True:
-
     address = input('Enter location: ')
     if len(address) < 1: break
 
-    url = serviceurl + urllib.parse.urlencode({'address': address})
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
 
     print('Retrieving', url)
-    uh = urllib.request.urlopen(url)
+    uh = urllib.request.urlopen(url, context=ctx)
     data = uh.read().decode()
     print('Retrieved', len(data), 'characters')
 
-try:
-    js = json.loads(data)
+    try:
+        js = json.loads(data)
+    except:
+        js = None
 
-except:
-    js = None
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
 
-if not js or 'status' not in js or js['status'] != 'OK':
-    print('==== Failure To Retrieve ====')
-    print(data)
-    continue
+    print(json.dumps(js, indent=4))
 
-print(json.dumps(js, indet=4))
-
-lat = js['results'][0]['geometry']['location']['lat']
-lng = js['results'][0]['geometry']['location']['lng']
-print('lat', lat, 'lng', lng)
-location
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
