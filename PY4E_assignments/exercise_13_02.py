fa84e61235918cdc853d926aedaee02df7c28247
Rhuck1 <bobbyhuck@gmail.com>
2022-11-26 20:37:07 -0700

    need to shower

--- PY4E_assignments/exercise_13_02.py
+++ PY4E_assignments/exercise_13_02.py
@@ -17,7 +17,8 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = print('Enter URL: ')
+
+url = input('Enter URL: ')
 html = urllib.request.urlopen(url, context=ctx).read()
 data = json.loads(html)
 
