14afc7074ce42375cd74086db31457bec21a939b
Rhuck1 <bobbyhuck@gmail.com>
2022-12-26 16:47:33 -0700

    working on ch 15

---
+++ PY4E_assignments/exercise_15/emaildb.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+DROP TABLE IF EXISTS Counts''')
+
+cur.execute('''
+CREATE TABLE Counts (email TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox-short.txt'
+
+fh = open(fname)
+
+for line in fh:
+
+    
+
