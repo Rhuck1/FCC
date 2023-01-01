a74f343dfb36929034a728c63731ed12f972219c
Rhuck1 <bobbyhuck@gmail.com>
2023-01-01 09:07:05 -0700

    15 02

--- PY4E_assignments/exercise_15/exercise_15_02/exercise_15_02.py
+++ PY4E_assignments/exercise_15/exercise_15_02/exercise_15_02.py
@@ -29,4 +29,21 @@ for line in fh:
     pieces = line.split()
     email = pieces[1]
 
-    
+    cur.execute('SELECT count from FROM Counts WHERE email = ?', (email, ))
+    row = cur.fetchone()
+
+    if row is None:
+        cur.execute('''INSERT INTO Coutns (email, count)
+            VALUES (?, 1)''', (email, ))
+
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?', (email, ))
+
+    conn.commit()
+
+sqlstr = 'SELECT email, count from Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
