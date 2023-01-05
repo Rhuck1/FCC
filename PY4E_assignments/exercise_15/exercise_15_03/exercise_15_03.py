377f0dfe15931f01a4041762b4494de6327d72d6
Rhuck1 <bobbyhuck@gmail.com>
2023-01-04 22:47:02 -0700

    ch 15 03

--- PY4E_assignments/exercise_15/exercise_15_03/exercise_15_03.py
+++ PY4E_assignments/exercise_15/exercise_15_03/exercise_15_03.py
@@ -65,7 +65,7 @@ CREATE TABLE Track (
     title  TEXT UNIQUE,
     album_id  INTEGER,
     genre_id  INTEGER,
-    len  INTEGER, rating  INTEGER, count INTEGER
+    len  INTEGER, rating  INTEGER, count  INTEGER
 );
 ''')
 
@@ -80,7 +80,52 @@ def lookup(d, key):
 
     found = False
     for child in d:
-        
 
+        if found: return child.text
 
+        if child.tag == 'key' and child.text == key:
+            found = True
 
+    return None
+
+stuff = ET.parse(fname)
+all = stuff.findall('dict/dict/dict')
+print('Dict count:', len(all))
+
+for entry in all:
+
+    if (lookup(entry, 'Track ID') is None): continue
+
+    name = lookup(entry, 'Name')
+    artist = lookup(entry, 'Artist')
+    album = lookup(entry, 'Album')
+    genre = lookup(entry, 'Genre')
+    count = lookup(entry, 'Play Count')
+    rating = lookup(entry, 'Rating')
+    length = lookup(entry, 'Total Time')
+
+    if name is None or artist is None or album is None: continue
+
+    print(name, artist, album, count, rating, length)
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+        Values (?)''', (artist, ))
+    cur.execute('SELECT id FROM Artist WHERE name = ?', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+        Values (?)''', (genre, ))
+    cur.execute('SELECT id FROM Artist WHERE name = ?', (genre, ))
+    genre_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+        Values (?, ?)''', (album, artist_id))
+    cur.execute('SELECT id FROM Album WHERE title = ?', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track 
+        (title, album_id, genre_id, len, rating, count) 
+        VALUES (?, ?, ?, ?, ?, ?)''', 
+        (name, album_id, genre_id, length, rating, count))
+
+    conn.commit()
