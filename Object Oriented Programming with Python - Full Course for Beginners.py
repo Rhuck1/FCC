6b7003d7e4d7446ed0853432ae8976752536d3c8
Rhuck1 <bobbyhuck@gmail.com>
2023-07-26 23:59:06 -0700

    late night

--- Object Oriented Programming with Python - Full Course for Beginners.py
+++ Object Oriented Programming with Python - Full Course for Beginners.py
@@ -5,7 +5,13 @@ In this complete tutorial, I will learn all about OOP and how to implement it us
 Instructional video link: https://www.youtube.com/watch?v=Ej_02ICOIgs
 '''
 
+import csv
+
 class Item:
+
+    pay_rate = 0.8 # The pay rate after 20% discount
+
+    all = []
 
     def __init__(self, name: str, price: float, quantity=0):
         
@@ -18,13 +24,46 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
 
         return self.price * self.quantity
+    
+    def apply_discount(self):
 
-item1 = Item("Phone", 100, 1)
+        self.price = self.price * self.pay_rate
 
-item2 = Item("Laptop", 1000, 3)
+    @classmethod
+    def instantiate_from_csv(cls):
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity'))
+            )
+
+    @staticmethod
+    def is_integer(num):
+        # We will count out the floats that are point zero (i.e. 5.0, 10.0)
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+    def __repr__(self):
+        return f"Item('{self.name}', '{self.price}', '{self.quantity}')"
+    
+
+Item.instantiate_from_csv()
+
+print(Item.all)
