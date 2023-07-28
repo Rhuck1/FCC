d98a530b82158991091b3f2089e3a952c0338a30
Rhuck1 <bobbyhuck@gmail.com>
2023-07-27 20:43:01 -0700

    finished polygon area calculator

--- Scientific_Computing_with_Python_Projects/Polygon_Area_Calculator/shape_calculator.py
+++ Scientific_Computing_with_Python_Projects/Polygon_Area_Calculator/shape_calculator.py
@@ -8,7 +8,7 @@ class Rectangle:
         # Assign to self object
         self.height = height
         self.width = width
-        print('initialization completed')
+        
 
     def set_width(self, width):
         self.width = width
@@ -27,46 +27,66 @@ class Rectangle:
 
     def get_picture(self):
         if self.width > 50 or self.height > 50:
-            return "Too big for picture"
+            return "Too big for picture."
         else:
             row = '*' * self.width
             rows = (row + '\n') * self.height
             return rows
 
-    def get_amount_inside(self):
-        pass
+    def get_amount_inside(self, polygon):
+        return self.get_area() // polygon.get_area()
+    
+    def __repr__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
 
 class Square(Rectangle):
     
     def __init__(self, side):
 
-        super().__init__()        )
+        super().__init__(side, side)
 
         # Assign to self object
         self.side = side
 
-rec1 = Rectangle(5, 4)
-print(rec1.height)
-print(rec1.width)
-print(rec1.get_area())
-print(rec1.get_perimeter())
-print(rec1.get_diagonal())
-print(rec1.get_picture())
+    def set_side(self, side):
+        self.width = side
+        self.height = side
 
-rec1.set_width(10)
-rec1.set_height(8)
+    def __repr__(self):
+        return f"Square(side={self.width})"
+    
 
-print(rec1.height)
-print(rec1.width)
-print(rec1.get_area())
-print(rec1.get_perimeter())
-print(rec1.get_diagonal())
-print(rec1.get_picture())
 
-sq1 = Square(4)
-print(sq1.height)
-print(sq1.width)
-print(sq1.get_area())
-print(sq1.get_perimeter())
-print(sq1.get_diagonal())
-print(sq1.get_picture())
+# rec1 = Rectangle(51, 4)
+# rec2 = Rectangle(2, 2)
+# print(rec1.height)
+# print(rec1.width)
+# print(rec1.get_area())
+# print(rec1.get_perimeter())
+# print(rec1.get_diagonal())
+# print(rec1.get_picture())
+# print(rec1.get_amount_inside(rec2))
+
+# rec1.set_width(10)
+# rec1.set_height(8)
+
+# print(rec1.height)
+# print(rec1.width)
+# print(rec1.get_area())
+# print(rec1.get_perimeter())
+# print(rec1.get_diagonal())
+# print(rec1.get_picture())
+
+# sq1 = Square(4)
+# print(sq1)
+# print(sq1.get_perimeter())
+# print(sq1.get_diagonal())
+# print(sq1.get_picture())
+
+# sq1.set_side(8)
+# print(sq1.get_area())
+# print(sq1)
+# print(sq1.get_area())
+# print(sq1.get_perimeter())
+# print(sq1.get_diagonal())
+# print(sq1.get_picture())
