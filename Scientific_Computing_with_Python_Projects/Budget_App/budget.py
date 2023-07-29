3253c7df05ad1eab7b2b891dd0d993083389d82e
Rhuck1 <bobbyhuck@gmail.com>
2023-07-29 16:36:38 -0700

    harder than the other project

--- Scientific_Computing_with_Python_Projects/Budget_App/budget.py
+++ Scientific_Computing_with_Python_Projects/Budget_App/budget.py
@@ -4,6 +4,7 @@ class Category:
 
     items = []
     ledger = []
+    balance = 0.00
 
     def __init__(self, name):
         print("Initialization successful")
@@ -13,14 +14,20 @@ class Category:
         # Actions to execute
         Category.items.append(self)
 
-    def deposit(self, amount, description=" "):
-        ledger.append({"amount": amount, "description": description})
+    def deposit(self, amount, description=""):
+        Category.ledger.append({"amount": amount, "description": description})
+        Category.balance += float(amount)
 
-    def withdraw(self):
-        pass
+    def withdraw(self, amount, description=""):
+        if Category.balance + (-amount) > 0:
+             Category.ledger.append({"amount": (-amount), "description": description})
+             Category.balance += float(-amount)
+             return True
+        else:
+            return False
 
     def get_balance(self):
-        pass
+        return Category.balance
 
     def transfer(self):
         pass
@@ -38,4 +45,8 @@ def create_spend_chart(categories):
     pass
 
 cat1 = Category("food")
-print(Category.all)
+cat1.deposit(250)
+cat1.withdraw(10, "cats")
+print(cat1.ledger)
+print(cat1.balance)
+print(cat1.get_balance())
