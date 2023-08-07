ed8c5c4405b7e57a1f1dbed648362c17feec86d1
Rhuck1 <bobbyhuck@gmail.com>
2023-08-07 16:38:31 -0700

    .....

--- Scientific_Computing_with_Python_Projects/Budget_App/budget.py
+++ Scientific_Computing_with_Python_Projects/Budget_App/budget.py
@@ -1,7 +1,6 @@
 class Category:
 
     def __init__(self, category):
-        print("Initialization successful")
         # Assign to self object
         self.category = category
         self.ledger = []
@@ -27,44 +26,48 @@ class Category:
             return True 
 
     def transfer(self, amount, budget_category):
-        if amount > self.get_balance():
+        if self.withdraw(amount, f"Transer to {budget_category.category}"):
             return False
         else:
-            self.withdraw(amount, "Transfer to " + category.category)
-            self.deposit(amount, f"Transfer from " + self.category)
+            budget_category.deposit(amount, f"Transfer from {budget_category.category}")
             return True
 
     def __repr__(self):
-        title_line = self.category.center(30, "*")
-        
-        return title_line
+        total = f"Total: " + f"{self.get_balance():.2f}"
+        header = f"{self.category:*^30}\n"
+        ledger = ""
+        for item in self.ledger:
+            description = f"{item['description']:<23}" 
+            amount = f"{item['amount']:>7.2f}"
+            ledger += f"{description[:23]}{amount[:7]}\n"
 
+        return header + ledger + total
 
 def create_spend_chart(categories):
     pass
 
 
-cat1 = Category("food")
-cat2 = Category("entertainment")
+# cat1 = Category("food")
+# cat2 = Category("entertainment")
 
-cat1.deposit(250)
-cat2.deposit(1000)
-
-print(cat1.get_balance())
-print(cat2.get_balance())
-print(cat1.check_funds(260))
-
-# print(cat2.transfer(200, "food"))
+# cat1.deposit(250)
+# cat2.deposit(1000)
 
 # print(cat1.get_balance())
 # print(cat2.get_balance())
 
-# cat1.transfer(100, 'clothing')
+
+# # print(cat2.transfer(200, "food"))
+
+# # print(cat1.get_balance())
+# # print(cat2.get_balance())
+
+# # cat1.transfer(100, 'clothing')
 
 
 
-# print(cat1.withdraw(300, "cats"))
-# print(cat2.withdraw(20, "turkey meatz"))
+# # print(cat1.withdraw(300, "cats"))
+# # print(cat2.withdraw(20, "turkey meatz"))
 
-# print(cat1)
+# # print(cat1)
 
