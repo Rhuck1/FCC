71e2c7952b8b55ce46d0b46c8b2f4d25c3f17829
Rhuck1 <bobbyhuck@gmail.com>
2023-08-07 15:02:23 -0700

    ...

--- Scientific_Computing_with_Python_Projects/Budget_App/budget.py
+++ Scientific_Computing_with_Python_Projects/Budget_App/budget.py
@@ -1,43 +1,42 @@
 class Category:
 
-    def __init__(self, description):
+    def __init__(self, category):
         print("Initialization successful")
         # Assign to self object
-        self.description = description
+        self.category = category
         self.ledger = []
-        self.balance = 0.0
 
     def deposit(self, amount, description=""):
         self.ledger.append({"amount": amount, "description": description})
-        self.balance += amount
 
     def withdraw(self, amount, description=""):
-        if self.balance + (-1.0 * amount) > 0:
+        if self.get_balance() >= amount:
              self.ledger.append({"amount": (-1.0 * amount), "description": description})
-             self.balance += (-1.0 * amount)
              return True
         else:
             return False
 
     def get_balance(self):
-        return self.balance
+        tot_balance = float(sum([item['amount'] for item in self.ledger]))
+        return tot_balance
     
     def check_funds(self, amount):
-        if amount >= self.get_balance():
+        if amount > self.get_balance():
             return False
         else:
             return True 
 
-    def transfer(self, amount, category):
+    def transfer(self, amount, budget_category):
         if amount > self.get_balance():
             return False
         else:
-            self.withdraw(amount, f"Transfer to {category.description}")
-            self.deposit(amount, f"Transfer from {self.description}")
+            self.withdraw(amount, "Transfer to " + category.category)
+            self.deposit(amount, f"Transfer from " + self.category)
             return True
 
     def __repr__(self):
-        title_line = self.catdescriptionegory.center(30, "*")
+        title_line = self.category.center(30, "*")
+        
         return title_line
 
 
@@ -51,20 +50,21 @@ cat2 = Category("entertainment")
 cat1.deposit(250)
 cat2.deposit(1000)
 
-print(cat1.withdraw(300, "cats"))
-print(cat2.withdraw(20, "turkey meatz"))
-
 print(cat1.get_balance())
 print(cat2.get_balance())
-
-print(cat2.transfer(200, "food"))
-
-print(cat1.get_balance())
-print(cat2.get_balance())
-
-cat1.transfer(100, 'clothing')
-
 print(cat1.check_funds(260))
 
-print(cat1)
+# print(cat2.transfer(200, "food"))
+
+# print(cat1.get_balance())
+# print(cat2.get_balance())
+
+# cat1.transfer(100, 'clothing')
+
+
+
+# print(cat1.withdraw(300, "cats"))
+# print(cat2.withdraw(20, "turkey meatz"))
+
+# print(cat1)
 
